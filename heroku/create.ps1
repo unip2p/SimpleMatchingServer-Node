0124afc0465e1b394b05cc4088002639e0c7cd3a
IrosSoftware <iros0522@gmail.com>
2019-05-01 13:11:59 +0900

    Publish Heroku

--- heroku/create.ps1
+++ heroku/create.ps1
@@ -1,4 +1,24 @@
-heroku create ("unip2p-"+([Guid]::NewGuid()).ToString().split("-")[0])
-git add .
+$UUID = ([Guid]::NewGuid()).ToString().split("-")[0]
+$URI = unip2p-$UUID
+heroku create $URI
+
+git remote set-url heroku https://git.heroku.com/${URI}.git
+git add ../
 git commit -m "Publish Heroku"
 git push heroku master
+
+$GameKey = ([Guid]::NewGuid()).ToString()
+$SecretKey = ([Guid]::NewGuid()).ToString()
+
+heroku config:set GAMEKEY=$GameKey
+heroku config:set SERCETKEY=$SecretKey
+
+Write-Output MatchingServerURL:$URI > ./url
+Write-Output MatchingGameKey:$GameKey  > ./key
+Write-Output MatchingSecretKey:$SecretKey > ./key
+
+Write-Output MatchingServerURL:https://$URI.herokuapp.com
+Write-Output MatchingGameKey:$GameKey
+Write-Output MatchingSecretKey:$SecretKey
+
+open https://$URI.herokuapp.com/$GameKey
